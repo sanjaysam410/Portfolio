f87d8bee8aa0250dc1c61e2bf3bccb339e29c5f1
sanjaysamala <sanjaysamala4100@gmail.com>
2026-02-11 23:20:13 +0530

    Fix: Remove deprecated images.domains in favor of remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "3000",
         pathname: "/assets/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.vercel.app",
       },
     ],
   },
